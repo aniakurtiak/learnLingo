199d6d3073ad2f0a12fba6a883067343ba44a1b5
Ania <124797307+aniakurtiak@users.noreply.github.com>
2024-02-10 01:25:16 +0200

    add GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    primaryBlack: '#121417',
+    primaryGray: '#8A8A89',
+    primaryYellow:'#F4C550',
+    primaryLightYellow: '#FFDC86',
+    lightYellow: '#FBE9BA',
+    green: '#9FBAAE',
+    lightGreen: '#CBDED3',
+    blue: '#9FB7CE',
+    lightBlue: '#BFD6EA',
+    pink: '#E0A39A',
+    lightPink: '#F2C0BD',
+    orange: '#F0AA8D',
+    lightOrange: '#F4C8BA',
+    }
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/learnLingo">
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
